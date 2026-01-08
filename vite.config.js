ec0c477d63d0f6bdb7d2af48b06980ef1c8961df
jattiphrswan <jattiphrswan49@gmail.com>
2026-01-08 10:37:36 +0530

    Fix image paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Jagmohan-portfolio/",
   plugins: [react()],
+  base: "/Jagmohan-portfolio/",
 });
